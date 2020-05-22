131ea4bedc75e3aef664990a5c00a61b59505537
Amouthinie <akulatheepan@gmail.com>
2020-05-22 13:36:34 +0530

    initial commit

---
+++ .history/client/src/components/App_20200522001005.js
@@ -0,0 +1,35 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Auth from "../hoc/auth";
+import LandHomePageingPage from "./views/LandingPage/HomePage.js";
+import LoginPage from "./views/LoginPage/LoginPage.js";
+import RegisterPage from "./views/UserRegistrationPage/UserRegisterationPage.js";
+import NavBar from "./views/NavBar/NavBar";
+import Footer from "./views/Footer/Footer"
+import ProductUploadPage from './views/ProductUploadPage/ProductUploadPage'
+import ProductDetailPage from './views/ProductDetailPage/ProductDetailPage';
+import ShoppingCartPage from './views/CartPage/ShoppingCartPage';
+import HistoryPage from './views/HistoryPage/History';
+
+function App() {
+  return (
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <NavBar />
+      <div style={{ paddingTop: '75px', minHeight: 'calc(100vh - 80px)' }}>
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
+          <Route exact path="/product/:productId" component={Auth(DetailProductPage, null)} />
+          <Route exact path="/user/cart" component={Auth(CartPage, true)} />
+          <Route exact path="/history" component={Auth(HistoryPage, true)} />
+
+        </Switch>
+      </div>
+      <Footer />
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ .history/server/config/dev_20200521194335.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoDbURI : 'mongodb+srv://fashion-store:fashion-store@db-5dk1r.mongodb.net/test?retryWrites=true&w=majority'
+}
