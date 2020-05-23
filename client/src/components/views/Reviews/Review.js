1481f5c695609d65bb0b17463053856783982f96
ruklani <ruklani.blogger@gmail.com>
2020-05-23 20:48:51 +0530

    Rating component added

--- client/src/components/views/Reviews/Review.js
+++ client/src/components/views/Reviews/Review.js
@@ -1,5 +1,6 @@
 import React,{Component} from "react";
 import { Divider ,Form, Input, InputNumber, Button } from "antd";
+import Rating from "../StarRating/Rating";
 
   const onFinish = values => {
     console.log(values);
@@ -11,9 +12,11 @@ const Review = () => {
      <div>
          <Divider/>
          <h3><b>Product Review</b></h3>
-      <Form name="review form" onFinish={onFinish} >
-
-        
+        <Form name="review form" onFinish={onFinish} >
+        <b>Rate the Product</b>
+        <Rating />
+        <br/>
+        <b>Comment</b>
         <Form.Item name={['user', 'review']} >
           <Input.TextArea />
         </Form.Item>
