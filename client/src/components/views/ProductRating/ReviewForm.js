d3628d31e6096d015562d00461031b5fe7f173d6
ruklani <ruklani.blogger@gmail.com>
2020-05-23 14:09:03 +0530

    wishlist button added

---
+++ client/src/components/views/ProductRating/ReviewForm.js
@@ -0,0 +1,43 @@
+// import React, { Component } from "react";
+// // import { Form, Button } from "react-bootstrap";
+// import { Formik } from 'formik';
+
+// class ReviewForm extends Component {
+//     render() {
+//         return (
+//             <div className="review-form">
+//                 <Form> 
+
+
+//                     {/* Name  */}
+                    
+//                     <Form.Group controlId="name">
+//                         <Form.Label>Name</Form.Label>
+//                         <Form.Control type="input" placeholder="Enter your name" />
+//                     </Form.Group>
+
+//                     {/* Date */}
+//                     <Form.Group controlId="datePosted">
+//                         <Form.Label>Date</Form.Label>
+//                         <Form.Control type="date" />
+//                     </Form.Group>
+
+
+//                     {/* Comment */}
+//                     <Form.Group  controlId="comment">
+//                         <Form.Label>Comment</Form.Label>
+//                         <Form.Control as="textarea" rows="3" placeholder="Add your comment...." />
+//                     </Form.Group>
+
+
+//                     <Button variant="primary" type="submit">
+//                         POST
+//                     </Button>
+//                 </Form>
+//             </div>
+//         )
+//     }
+
+// }
+
+// export default ReviewForm;
