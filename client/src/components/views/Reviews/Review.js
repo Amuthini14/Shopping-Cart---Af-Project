d501c8645894fcd7ef989c1ff14979dae083cad4
ruklani <ruklani.blogger@gmail.com>
2020-05-24 12:11:16 +0530

    Updated Review Component

--- client/src/components/views/Reviews/Review.js
+++ client/src/components/views/Reviews/Review.js
@@ -1,44 +1,64 @@
-import React,{Component} from "react";
-import { Divider ,Form, Input, InputNumber, Button } from "antd";
+import React, { Component } from "react";
+import { Divider, Form, Input, InputNumber, Button } from "antd";
 import Rating from "../StarRating/Rating";
+import axios from 'axios';
 
-  const onFinish = values => {
-    console.log(values);
-  };
+class Review extends Component {
+  constructor(props) {
+    super(props);
 
-
-  class Review extends Component{
-    constructor(props){
-      super(props);
-
-      this.state={
-        
-      }
-    }
-    render(){
-      return(
-        <div>
-        <Divider/>
-        <h3><b>Product Review</b></h3>
-       <Form name="review form" onFinish={onFinish} >
-       <b>Rate the Product</b>
-       <Rating />
-       <br/>
-       <b>Comment</b>
-       <Form.Item name={['user', 'review']} >
-         <Input.TextArea />
-       </Form.Item>
-       <Form.Item>
-         <Button  htmlType="submit" type="primary">
-           Post
-         </Button>
-       </Form.Item>
-     </Form>
-     <Divider/>
-     </div>
-    
-      )
+    this.state = {
+       rating:'',
+       comment:''
     }
   }
+  onChangeRating=(e)=>{
+    this.setState({ rating: e.target.value })
+  }
+
+  onChangeComment=(e)=>{
+    this.setState({ comment: e.target.value })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+
+   let reviewObj = {
+      rating: this.state.rating,
+      comment: this.state.comment,
+    };
+    axios.post('http://localhost:5000/add-review', reviewObj)
+      .then(res => console.log(res.data));
+
+    this.setState({ rating: '', comment: ''})
+  };
+  
+
+
+  render() {
+    return (
+      <div>
+        <Divider />
+        <h3><b>Product Review</b></h3>
+        <Form name="review form" onFinish={this.onSubmit} >
+          <b>Rate the Product</b>
+          <Rating onChange={this.onChangeRating}/>
+          <br />
+          <b>Comment</b>
+          <Form.Item name={['user', 'review']} onChange={this.onChangeComment} >
+            <Input.TextArea />
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="submit" type="primary">
+              Post
+         </Button>
+          </Form.Item>
+        </Form>
+        <Divider />
+      </div>
+
+    )
+  }
+}
 
 export default Review;
